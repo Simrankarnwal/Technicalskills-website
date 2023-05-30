0225a7cf2ed3ab688f24d478d846dca36ec0d2c9
Simrankarnwal <simrankarnwal111@gmail.com>
2023-05-30 13:55:10 +0530

    Technicalskills responsive website

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='w-100 h-50 bg-light text-center'>
+        <p>@2023 Technical skills. All Rights Reserved | Terms and Conditions </p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ Sdata.jsx
@@ -0,0 +1,36 @@
+import web from "../src/images/img454.jpg";
+import app from "../src/images/img00.jpg";
+import android from "../src/images/img45.jpg";
+import software from "../src/images/img676.jpg"
+import digital from "../src/images/img78.jpg";
+import marketing from "../src/images/img87.jpg";
+
+const Sdata =[
+    {
+        imgsrc: web,
+        title: "Web development",
+    },
+
+    {
+        imgsrc: app,
+        title: "App development",
+    },
+    {
+        imgsrc: software,
+        title: "Software development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital development",
+    },
+    {
+        imgsrc: marketing,
+        title: "Marketing development",
+    },
+    {
+        imgsrc: android,
+        title: "Android development",
+    },
+];
+
+export default Sdata;
